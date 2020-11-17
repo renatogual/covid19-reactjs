ee2a554a292e7fded39df37a31c50b0f2d784404
eox-tecnologia <eox-tecnologia>
2020-11-17 13:30:35 -0300

    Criado container principal main

---
+++ src/containers/Main/index.jsx
@@ -0,0 +1,28 @@
+import React, { memo, useCallback, useEffect, useState } from 'react' //O memo evita re-renderizar a pagina, utilizado em componentes que só precisam ser renderizados uma vez
+import Api from '../../api'
+import { ContainerStyled } from './style'
+
+function Main(){
+    const [data, setData] = useState({})
+    const [country, setCountry] = useState('brazil')
+
+    //Memoriza o valor, caso não seja passado outro valor diferente no parametro country
+    const getCovidData = useCallback((country) => {
+        Api.getCountry(country)
+            .then((data) => setData(data))
+    }, [])
+
+    useEffect(() => {
+        getCovidData(country)
+    }, [getCovidData, country])
+
+    return(
+        <ContainerStyled>
+            <div className="mb-2">
+
+            </div>
+        </ContainerStyled>
+    )
+}
+
+export default memo(Main)

ead3f3023fac55c57fef032303b79364679b0356
renatogual <renatogual@hotmail.com>
2020-11-18 20:31:10 -0300

    Criado constante de paises do componente select

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,35 @@
+import BrazilFlag from '../../assets/img/brazil.png'
+import ChinaFlag from '../../assets/img/china.png'
+import GermanyFlag from '../../assets/img/germany.png'
+import ItalyFlag from '../../assets/img/italy.png'
+import UsaFlag from '../../assets/img/usa.png'
+
+const COUNTRIES = [
+    {
+        label: 'Brasil',
+        value: 'brazil',
+        flag: BrazilFlag
+    },
+    {
+        label: 'China',
+        value: 'china',
+        flag: ChinaFlag
+    },
+    {
+        label: 'Alemanha',
+        value: 'germany',
+        flag: GermanyFlag
+    },
+    {
+        label: 'Itália',
+        value: 'italy',
+        flag: ItalyFlag
+    },
+    {
+        label: 'Estados Unidos',
+        value: 'usa',
+        flag: UsaFlag
+    },
+]
+
+export default COUNTRIES

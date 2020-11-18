f394c55b11fd0e613a5c5c6e2f5010e9f1de0716
renatogual <renatogual@hotmail.com>
2020-11-18 20:54:08 -0300

    Implementando componente panel na pagina principal

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -2,10 +2,12 @@ import React, { memo, useCallback, useEffect, useState } from 'react' //O memo e
 import Api from '../../api'
 import { ContainerStyled } from './style'
 import Board from './components/Board'
+import Panel from './components/Panel'
 
 function Main(){
     const [data, setData] = useState({})
     const [country, setCountry] = useState('brazil')
+    const updateAt = new Date().toLocaleString()
 
     //Memoriza o valor, caso não seja passado outro valor diferente no parametro country
     const getCovidData = useCallback((country) => {
@@ -17,10 +19,21 @@ function Main(){
         getCovidData(country)
     }, [getCovidData, country])
 
+    const handleChange = ({target}) => {
+        const country = target.value
+        setCountry(country)
+    }
+
     return(
         <ContainerStyled>
             <div className="mb-2">
-
+                <Panel 
+                    data={data}
+                    updateAt={updateAt}
+                    onChange={handleChange}
+                    country={country}
+                    getCovidData={getCovidData}
+                />
             </div>
             <Board data={data} />
         </ContainerStyled>
